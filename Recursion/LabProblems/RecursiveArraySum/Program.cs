b5b933be94664f2166995edd3b71ddc2563ba07b
radomirKrastev <radomirkostantinov@gmail.com>
2019-07-17 15:48:53 +0300

    Recursion problem

---
+++ Recursion/LabProblems/RecursiveArraySum/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace RecursiveArraySum
+{
+    using System;
+    using System.Linq;
+    public class Program
+    {
+        public static void Main()
+        {
+            var array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var sum = Sum(array, 0);
+            Console.WriteLine(sum);
+        }
+
+        public static int Sum(int[] array, int index)
+        {
+            if (index == array.Length)
+            {
+                return 0;
+            }
+
+            var sum = array[index] + Sum(array, index + 1);
+            return sum;
+        }
+    }
+}
