0a3458edf27779caedd4d877ba62a094d493eee4
radomirKrastev <radomirkostantinov@gmail.com>
2019-07-17 18:49:06 +0300

    Recursion problem

---
+++ Recursion/LabProblems/GeneratingVectors/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace GeneratingVectors
+{
+    using System;
+    public class Program
+    {
+        public static void Main()
+        {
+            var length = int.Parse(Console.ReadLine());
+            var array = new int[length];
+            PrintVector(0, array);
+        }
+
+        public static void PrintVector(int index, int [] array)
+        {
+            if (index == array.Length)
+            {
+                Console.WriteLine(string.Join("", array));
+            }
+
+            else
+            {
+                for (int i = 0; i <= 1; i++)
+                {
+                    array[index] = i;
+                    PrintVector(index + 1, array);
+                }
+            }            
+        }
+
+    }
+}
