ad8539957a5b71990bc8717574d1b4e6fb7d1dd9
radomirKrastev <radomirkostantinov@gmail.com>
2019-07-17 17:02:48 +0300

    Recursion problem

---
+++ Recursion/LabProblems/RecursiveDrawing/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace RecursiveDrawing
+{
+    using System;
+    public class Program
+    {
+        public static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+            var originalNumber = number;
+
+            Print(number);
+        }
+
+        public static void Print(int number)
+        {
+            if (number==0)
+            {
+                return;
+            }
+
+            Console.WriteLine(new string('*', number));
+            Print(number - 1);
+            Console.WriteLine(new string('#',number));
+        }
+    }
+}
