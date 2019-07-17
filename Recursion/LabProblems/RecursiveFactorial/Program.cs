ae5a97a5b1d7cc3dee2edc732425c7fbafc43992
radomirKrastev <radomirkostantinov@gmail.com>
2019-07-17 16:15:50 +0300

    Recursion problem

---
+++ Recursion/LabProblems/RecursiveFactorial/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace RecursiveFactorial
+{
+    using System;
+    public class Program
+    {
+        public static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+            var factorial = GetFactorial(number);
+            Console.WriteLine(factorial);
+        }
+
+        public static long GetFactorial (int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            var factorial = number * GetFactorial(number-1);
+            return factorial;
+        }
+    }
+}
