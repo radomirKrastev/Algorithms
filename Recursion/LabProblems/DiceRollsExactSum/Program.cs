36b82bc23a3c526daa5f303f66b48082ba7b1d27
radomirKrastev <radomirkostantinov@gmail.com>
2019-07-21 18:27:38 +0300

    Recursion and backtracking problems

---
+++ Recursion/LabProblems/DiceRollsExactSum/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace DiceRolls
+{
+    using System;
+    using System.Collections.Generic;
+    public class Program
+    {
+        public static void Main()
+        {
+            var dice = int.Parse(Console.ReadLine());
+            var rolledDices = new List<int>();
+            var desiredSum = int.Parse(Console.ReadLine());
+            var sumSoFar = 0;
+            PrintDiceRolls(dice, rolledDices, desiredSum, sumSoFar);
+        }
+
+        public static void PrintDiceRolls(int dice, List<int> rolledDices, int desiredSum, int sumSoFar)
+        {
+            if (dice==0)
+            {
+                if (sumSoFar == desiredSum)
+                {
+                    Console.WriteLine(string.Join(" ", rolledDices));
+                }                
+            }
+
+            else
+            {
+                for (int i = 1; i <= 6; i++)
+                {
+                    var minSum = sumSoFar + i + 1 * (dice - 1);
+                    var maxSum = sumSoFar + i + 6 * (dice - 1);
+
+                    if (desiredSum >= minSum && desiredSum <= maxSum)
+                    {
+                        //choose 
+                        rolledDices.Add(i);
+                        //search/explore
+                        PrintDiceRolls(dice - 1, rolledDices, desiredSum, sumSoFar + i);
+                        //un-choose
+                        rolledDices.RemoveAt(rolledDices.Count - 1);
+                    }                    
+                }
+            }
+        }
+    }
+}
---
+++ Recursion/LabProblems/SubLists/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace SubLists
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class Program
+    {
+        public static void Main()
+        {
+            var names = Console.ReadLine().Split().ToList();
+            var subListOfNames = new List <string>();
+            PrintSubList(names, subListOfNames);
+        }
+
+        public static void PrintSubList(List<string> names, List <string> subList)
+        {
+            if (names.Count==0)
+            {
+                Console.WriteLine(string.Join(" ",subList));
+            }
+
+            else
+            {
+                var name = names[0];
+                names.Remove(name);
+
+                subList.Add(name);
+                PrintSubList(names, subList);
+                subList.RemoveAt(subList.Count - 1);
+
+                PrintSubList(names, subList);
+                names.Insert(0, name);
+            }
+        }
+    }
+}
