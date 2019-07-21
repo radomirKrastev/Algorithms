6aea5c2d14d8eff9eb54637a51fe1139c2d2b7f3
radomirKrastev <radomirkostantinov@gmail.com>
2019-07-21 13:45:13 +0300

    Recursion and backtracking problem

---
+++ Recursion/LabProblems/DiceRolls/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace DiceRolls
+{
+    using System;
+    using System.Collections.Generic;
+    public class Program
+    {
+        public static void Main()
+        {
+            int dice = 3;
+            var rolledDices = new List<int>();
+            PrintDiceRolls(dice, rolledDices);
+        }
+
+        public static void PrintDiceRolls (int dice, List <int> rolledDices)
+        {
+            if (dice == 0)
+            {
+                Console.WriteLine(string.Join(" ", rolledDices));
+            }
+
+            else
+            {
+                for (int i = 1; i <= 6; i++)
+                {
+                    //choose 
+                    rolledDices.Add(i);
+                    //search/explore
+                    PrintDiceRolls(dice - 1, rolledDices);
+                    //un-choose
+                    rolledDices.RemoveAt(rolledDices.Count - 1);
+                }
+            }
+        }
+    }
+}
---
+++ Recursion/LabProblems/EightQueens/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace EightQueens
+{
+    using System;
+    public class Program
+    {
+        public static void Main()
+        {
+            var field = new int[64];
+            var queensPositioned = PlaceQueens(field);
+            
+
+            Console.WriteLine(queensPositioned);
+        }
+
+        public static int [] PlaceQueens (int[] field)
+        {
+            var isRowBeatable = true;
+            var isColumnBeatable = true;
+            var isDiagonalBeatable = true;
+
+
+
+            if(isRowBeatable==false && isColumnBeatable==false && isDiagonalBeatable == false)
+            {
+                return field;
+            }
+
+            else
+            {
+                for (int i = 0; i < field.Length; i++)
+                {
+                    for (int i = 0; i < field.Length; i++)
+                    {
+
+                    }
+                }
+
+            }
+        }
+    }
+}
